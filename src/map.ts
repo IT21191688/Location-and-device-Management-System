a6a60ef76cca92273aec3ee7235fb0f47bdc5555
IT21191688 <IT21191688@my.sliit.lk>
2024-04-04 22:12:58 +0530

    map completed

--- src/map.ts
+++ src/map.ts
@@ -1,8 +1,12 @@
 import constants from "./utills/constants";
 import UserRouter from "./routes/user-route";
+import DeviceRouter from "./routes/device-route";
+import LocationRouter from "./routes/location-route";
 
 const requestMappings = (app: any) => {
   app.use(constants.API.PREFIX.concat("/user"), UserRouter);
+  app.use(constants.API.PREFIX.concat("/device"), DeviceRouter);
+  app.use(constants.API.PREFIX.concat("/location"), LocationRouter);
 };
 
 export default requestMappings;

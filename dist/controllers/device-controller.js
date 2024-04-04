62e9fb5a3fddecd352418187b3ff49dfc4be8842
IT21191688 <IT21191688@my.sliit.lk>
2024-04-04 22:07:05 +0530

    device controller success

--- dist/controllers/device-controller.js
+++ dist/controllers/device-controller.js
@@ -1,1 +1,127 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DeleteDevice = exports.UpdateDevice = exports.FindDeviceById = exports.FindAllDevices = exports.CreateDevice = void 0;
+const http_status_codes_1 = require("http-status-codes");
+const device_service_1 = __importDefault(require("../services/device-service"));
+const user_service_1 = __importDefault(require("../services/user-service"));
+const NotFoundError_1 = __importDefault(require("../utills/error/error.classes/NotFoundError"));
+const responce_1 = __importDefault(require("../utills/responce"));
+const CreateDevice = async (req, res) => {
+    const body = req.body;
+    const auth = req.auth;
+    try {
+        const user = await user_service_1.default.findById(auth._id);
+        if (!user)
+            throw new NotFoundError_1.default("User not found!");
+        const createdDevice = await device_service_1.default.saveDevice(body, null);
+        (0, responce_1.default)(res, true, http_status_codes_1.StatusCodes.CREATED, "Device created successfully!", createdDevice);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(http_status_codes_1.StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Error creating device",
+            error: error.message,
+        });
+    }
+};
+exports.CreateDevice = CreateDevice;
+const FindAllDevices = async (req, res) => {
+    const auth = req.auth;
+    try {
+        const user = await user_service_1.default.findById(auth._id);
+        if (!user)
+            throw new NotFoundError_1.default("User not found!");
+        const allDevices = await device_service_1.default.findAllDevices();
+        (0, responce_1.default)(res, true, http_status_codes_1.StatusCodes.OK, "All devices retrieved successfully!", allDevices);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(http_status_codes_1.StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Error retrieving devices",
+            error: error.message,
+        });
+    }
+};
+exports.FindAllDevices = FindAllDevices;
+// Add other device controller functions here
+const FindDeviceById = async (req, res) => {
+    const deviceId = req.params.deviceId;
+    const auth = req.auth;
+    try {
+        const user = await user_service_1.default.findById(auth._id);
+        if (!user)
+            throw new NotFoundError_1.default("User not found!");
+        const device = await device_service_1.default.findDeviceById(deviceId);
+        if (!device) {
+            (0, responce_1.default)(res, false, http_status_codes_1.StatusCodes.NOT_FOUND, "Device not found", null);
+        }
+        else {
+            (0, responce_1.default)(res, true, http_status_codes_1.StatusCodes.OK, "Device retrieved successfully", device);
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.status(http_status_codes_1.StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Error retrieving device",
+            error: error.message,
+        });
+    }
+};
+exports.FindDeviceById = FindDeviceById;
+const UpdateDevice = async (req, res) => {
+    const deviceId = req.params.deviceId;
+    const updatedData = req.body;
+    const auth = req.auth;
+    try {
+        const user = await user_service_1.default.findById(auth._id);
+        if (!user)
+            throw new NotFoundError_1.default("User not found!");
+        const updatedDevice = await device_service_1.default.editDeviceDetails(deviceId, updatedData);
+        if (!updatedDevice) {
+            (0, responce_1.default)(res, false, http_status_codes_1.StatusCodes.NOT_FOUND, "Device not found", null);
+        }
+        else {
+            (0, responce_1.default)(res, true, http_status_codes_1.StatusCodes.OK, "Device updated successfully", updatedDevice);
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.status(http_status_codes_1.StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Error updating device",
+            error: error.message,
+        });
+    }
+};
+exports.UpdateDevice = UpdateDevice;
+const DeleteDevice = async (req, res) => {
+    const deviceId = req.params.deviceId;
+    const auth = req.auth;
+    try {
+        const user = await user_service_1.default.findById(auth._id);
+        if (!user)
+            throw new NotFoundError_1.default("User not found!");
+        const deletedDevice = await device_service_1.default.deleteDeviceById(deviceId);
+        if (!deletedDevice) {
+            (0, responce_1.default)(res, false, http_status_codes_1.StatusCodes.NOT_FOUND, "Device not found", null);
+        }
+        else {
+            (0, responce_1.default)(res, true, http_status_codes_1.StatusCodes.OK, "Device deleted successfully", null);
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.status(http_status_codes_1.StatusCodes.INTERNAL_SERVER_ERROR).json({
+            success: false,
+            message: "Error deleting device",
+            error: error.message,
+        });
+    }
+};
+exports.DeleteDevice = DeleteDevice;
---
+++ src/controllers/device-controller.ts
@@ -0,0 +1,180 @@
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import deviceService from "../services/device-service";
+import userService from "../services/user-service";
+import NotFoundError from "../utills/error/error.classes/NotFoundError";
+import CustomResponse from "../utills/responce";
+
+const CreateDevice = async (req: Request, res: Response) => {
+  const body = req.body;
+  const auth = req.auth;
+
+  try {
+    const user = await userService.findById(auth._id);
+    if (!user) throw new NotFoundError("User not found!");
+
+    const createdDevice = await deviceService.saveDevice(body, null);
+
+    CustomResponse(
+      res,
+      true,
+      StatusCodes.CREATED,
+      "Device created successfully!",
+      createdDevice
+    );
+  } catch (error: any) {
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Error creating device",
+      error: error.message,
+    });
+  }
+};
+
+const FindAllDevices = async (req: Request, res: Response) => {
+  const auth = req.auth;
+
+  try {
+    const user = await userService.findById(auth._id);
+    if (!user) throw new NotFoundError("User not found!");
+
+    const allDevices = await deviceService.findAllDevices();
+    CustomResponse(
+      res,
+      true,
+      StatusCodes.OK,
+      "All devices retrieved successfully!",
+      allDevices
+    );
+  } catch (error: any) {
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Error retrieving devices",
+      error: error.message,
+    });
+  }
+};
+
+// Add other device controller functions here
+
+const FindDeviceById = async (req: Request, res: Response) => {
+  const deviceId = req.params.deviceId;
+  const auth = req.auth;
+
+  try {
+    const user = await userService.findById(auth._id);
+    if (!user) throw new NotFoundError("User not found!");
+
+    const device = await deviceService.findDeviceById(deviceId);
+    if (!device) {
+      CustomResponse(
+        res,
+        false,
+        StatusCodes.NOT_FOUND,
+        "Device not found",
+        null
+      );
+    } else {
+      CustomResponse(
+        res,
+        true,
+        StatusCodes.OK,
+        "Device retrieved successfully",
+        device
+      );
+    }
+  } catch (error: any) {
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Error retrieving device",
+      error: error.message,
+    });
+  }
+};
+
+const UpdateDevice = async (req: Request, res: Response) => {
+  const deviceId = req.params.deviceId;
+  const updatedData = req.body;
+  const auth = req.auth;
+
+  try {
+    const user = await userService.findById(auth._id);
+    if (!user) throw new NotFoundError("User not found!");
+
+    const updatedDevice = await deviceService.editDeviceDetails(
+      deviceId,
+      updatedData
+    );
+    if (!updatedDevice) {
+      CustomResponse(
+        res,
+        false,
+        StatusCodes.NOT_FOUND,
+        "Device not found",
+        null
+      );
+    } else {
+      CustomResponse(
+        res,
+        true,
+        StatusCodes.OK,
+        "Device updated successfully",
+        updatedDevice
+      );
+    }
+  } catch (error: any) {
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Error updating device",
+      error: error.message,
+    });
+  }
+};
+
+const DeleteDevice = async (req: Request, res: Response) => {
+  const deviceId = req.params.deviceId;
+  const auth = req.auth;
+
+  try {
+    const user = await userService.findById(auth._id);
+    if (!user) throw new NotFoundError("User not found!");
+
+    const deletedDevice = await deviceService.deleteDeviceById(deviceId);
+    if (!deletedDevice) {
+      CustomResponse(
+        res,
+        false,
+        StatusCodes.NOT_FOUND,
+        "Device not found",
+        null
+      );
+    } else {
+      CustomResponse(
+        res,
+        true,
+        StatusCodes.OK,
+        "Device deleted successfully",
+        null
+      );
+    }
+  } catch (error: any) {
+    console.error(error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Error deleting device",
+      error: error.message,
+    });
+  }
+};
+
+export {
+  CreateDevice,
+  FindAllDevices,
+  FindDeviceById,
+  UpdateDevice,
+  DeleteDevice,
+};

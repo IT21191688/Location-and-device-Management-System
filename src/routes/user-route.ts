0231a33efe227c7bd56a77ac422250e7eb917ddb
IT21191688 <IT21191688@my.sliit.lk>
2024-04-04 21:02:14 +0530

    init

---
+++ src/routes/user-route.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import UserMiddleware from "../middlewares/user-middleware";
+
+import {
+  RegisterUser,
+  GetUserProfile,
+  UserLogin,
+} from "../controllers/user-controller";
+import constants from "../utills/constants";
+
+const UserRouter = Router();
+
+UserRouter.post("/register", RegisterUser);
+
+UserRouter.post("/login", UserLogin);
+
+UserRouter.get(
+  "/profile",
+  UserMiddleware.authorize([
+    constants.USER.ROLES.ADMIN,
+    constants.USER.ROLES.USER,
+  ]),
+  GetUserProfile
+);
+
+export default UserRouter;
---
+++ src/utills/constants.ts
@@ -0,0 +1,18 @@
+const constants = {
+  API: {
+    PREFIX: "/api/v1",
+  },
+
+  USER: {
+    ROLES: {
+      ADMIN: "admin",
+      USER: "user",
+    },
+  },
+
+  CLOUDINARY: {
+    FILE_NAME: "Device-Management-System",
+  },
+};
+
+export default constants;
